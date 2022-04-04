409503149e2b25c0f2883610bdabbc3d206faad2
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2022-04-05 02:26:44 +0900

    Update installer.ps1
    ・内部処理の見直し
    ・Scrapboxでの工程を一通り実装
    ・ついでにLuaJITも使用することにする

--- installer.ps1
+++ installer.ps1
@@ -6,89 +6,133 @@
 .SYNOPSIS
     AviUtlの環境を構築します
 .DESCRIPTION
-    https://scrapbox.io/aviutl/インストール に基づき、AviUtlと周辺プラグインを含めた環境を構築します。
+    https://scrapbox.io/aviutl/セットアップ に基づき、AviUtlと周辺プラグインを含めた環境を構築します。
+.PARAMETER InstallPath
+    インストール先のパスを指定します。デフォルトではカレントディレクトリ内のtestディレクトリが指定されます。
+.PARAMETER WorkingDirectry
+    作業用フォルダを指定します。デフォルトでは TEMP\aviutl が使用されます。
+.PARAMETER KeepForeignData
+    インストール後に作業用フォルダを保持する場合に使用します。
+.EXAMPLE
+    installer.ps1 C:\Path\To\Aviutl
 .LINK
-    https://scrapbox.io/aviutl/インストール
+    https://scrapbox.io/aviutl/セットアップ
 #>
+
+# 2022/04/04
+# 関数の戻り値を明確にするためにreturnを用いることとする
 
 param(
     [Parameter(Mandatory=$false,
     Position=0)]
     [String]
-    $installPath = "test"
+    $InstallPath = (New-Item 'test' -ItemType Directory -Force),
+    [Parameter()]
+    [String]
+    $WorkingDirectry = (New-Item (Join-Path $env:TEMP aviutl) -ItemType Directory -Force),
+    [Parameter()]
+    [Switch]
+    $KeepForeignData
 )
 
 Set-StrictMode -Version Latest
+Set-Location -Path $InstallPath
 
-if (!(Test-Path $installPath)) {
-    New-Item $installPath -ItemType Directory
+# ディレクトリが存在しない場合に作成してパスを返す
+function New-DirectryIfNotExist {
+    param (
+        [Parameter(Mandatory=$true,
+        Position=0)]
+        [String]
+        $Directry
+    )
+
+    if (Test-Path $Directry) {
+        return $Directry
+    } else {
+        return New-Item $Directry -ItemType Directory
+    }
 }
 
-[String]$path = Convert-Path -Path $installPath
-[String]$temp = "$path\_temp"
-[String]$plugins = "$path\plugins"
+# zipファイルを投げると解凍して中身を返す
+function Get-ArchiveItems {
+    param (
+        [Parameter(Mandatory=$true,
+        ValueFromPipeline=$true,
+        Position=0)]
+        [String]
+        $Archive,
 
-if (!(Test-Path $temp)) {
-    New-Item $temp -ItemType Directory
+        # 作業用のディレクトリ
+        [Parameter(Position=1)]
+        [String]
+        $Directry = $WorkingDirectry
+    )
+
+    # アーカイブ名のフォルダを解凍先に指定する
+    $DestinationPath = Join-Path $Directry ([System.IO.Path]::GetFileNameWithoutExtension($Archive))
+    Expand-Archive $Archive -DestinationPath $DestinationPath -Force
+    $items = Get-ChildItem $DestinationPath
+    # 1階層目に単一のフォルダが存在する場合はその中身を取得
+    # @()で囲うことで要素が一つの場合でも配列として扱う
+    # https://social.technet.microsoft.com/Forums/ja-JP/230719ec-e5a8-4b32-9cf9-b7bdec0b50c3/22793259681239526684320131237312428123901235612427389173044612?forum=powershellja
+    if (@($items).Count -eq 1 -and $items.PSIsContainer) {
+        return Get-ChildItem $items
+    } else {
+        return $items
+    }
 }
-if (!(Test-Path $plugins)) {
-    New-Item $plugins -ItemType Directory
-}
 
-
-[String]$aviutl = "http://spring-fragrance.mints.ne.jp/aviutl/aviutl110.zip"
-[String]$exedit = "http://spring-fragrance.mints.ne.jp/aviutl/exedit92.zip"
-[String]$lw = "https://api.github.com/repos/Mr-Ojii/L-SMASH-Works-Auto-Builds/releases"
-[String]$lw_KeyWord = "Mr-Ojii_AviUtl.zip"
-
-Set-Location -Path $path
-
+# Uriの末尾をもとにファイル名を取得する(URLでもパスでも可)
 Function Get-FileName($Uri) {
-    [System.IO.Path]::GetFileName($Uri)
+    return [System.IO.Path]::GetFileName($Uri)
 }
 
-function Get-IfNotExist {
+# URLを投げると出力先に存在しなければ取ってくる
+function Get-UriFile {
     param (
         [Parameter(Mandatory=$true,
         ValueFromPipeline=$true,
         Position=0)]
         [String]
         $Uri,
-        [Parameter(Mandatory=$true,
-        Position=1)]
+        [Parameter()]
         [String]
-        $DestinationPath
+        $DestinationPath = $WorkingDirectry
     )
     
     $o = Join-Path $DestinationPath (Get-FileName($Uri))
     if (!(Test-Path $o)) {
         Invoke-WebRequest $Uri -OutFile $o
     }
-    Convert-Path $o
+    return Convert-Path $o
 }
 
+# GitHubのReleasesに存在する最新のファイルのUrlを取得する
+#   第一引数: GitHubのReleasesを取得するAPIのURL (https://api.github.com/repos/<OWNER>/<REPOSITORY>/releases)
+#   第二引数: (省略可) 取得するファイルのキーワード (x86, AviUtl など)
 function Get-GithubLatestUrl {
     param (
-        [Parameter(
-            Mandatory=$true,
-            ValueFromPipeline=$true,
-            Position=0)]
-        [string]
+        [Parameter(Mandatory=$true,
+        ValueFromPipeline=$true,
+        Position=0)]
+        [String]
         $Uri,
         [Parameter(Position=1)]
         [String]
-        $KeyWord
+        $Keyword
     )
 
-    $Json = curl $Uri | ConvertFrom-Json
-    if($null -eq $KeyWord) {
-        $json.assets.browser_download_url[0].ToString()
+    $Json = Invoke-WebRequest $Uri | ConvertFrom-Json
+
+    if(!$Keyword) {
+        return $json.assets.browser_download_url[0].ToString()
     } else {
-        ($json.assets.browser_download_url | Select-String $KeyWord)[0].ToString()
+        return ($json.assets.browser_download_url | Select-String $KeyWord)[0].ToString()
     }
 }
 
-Function Install-Uri() {
+function Install-UriArchive {
     param(
         [Parameter(Mandatory=$true,
         ValueFromPipeline=$true,
@@ -96,38 +140,68 @@ Function Install-Uri() {
         [String]
         $Uri,
 
-        [Parameter(Position=1)]
-        [String]
-        $Type
-    )
-    $File = Get-IfNotExist $Uri $temp
-    if ("plugins" -eq $Type) {
-        Expand-Archive $File -DestinationPath $plugins -Force
-    } else {
-        Expand-Archive $File -DestinationPath $path -Force
-    }
-}
-
-function Install-Uri-lw {
-    param(
         [Parameter(Mandatory=$true,
-        ValueFromPipeline=$true,
-        Position=0)]
+        Position=1)]
         [String]
-        $Uri
+        $Path,
+
+        # インストール対象
+        [Parameter()]
+        [String]
+        $Filter,
+
+        # 対象外のファイル(ディレクトリ)に付ける接頭辞
+        # 指定した場合は対象外のファイルも接頭辞が付いた状態でインストールされる
+        [Parameter()]
+        [String]
+        $Prefix,
+
+        # Prefix指定時に除外するファイル(ディレクトリ)
+        [Parameter()]
+        [String]
+        $Exclude
     )
-    $File = Get-IfNotExist $Uri $temp
-    Expand-Archive -Force -Path $File -DestinationPath $temp\lw
-    Get-ChildItem $temp\lw -Filter *.au* |
-        Copy-Item -Force -Destination $plugins
-    Get-ChildItem $temp\lw -Exclude *.au*,Licenses |
-    ForEach-Object{
-        $Prefixed = "lw_" + $_.Name
-        Copy-Item -Force $_ -Destination $plugins\$Prefixed
+
+    $items = Get-UriFile $Uri | Get-ArchiveItems
+    
+    if (!$Filter) {
+        $items | Copy-Item -Destination $Path -Force
+    } else {
+        $items | Where-Object {$_.Name -like $Filter} |
+        Copy-Item -Destination $Path -Force
+
+        if ($Prefix) {
+            $items | Where-Object {$_.Name -notlike $Filter} | Where-Object {$_.Name -notlike $Exclude} |
+            ForEach-Object{
+                $Prefixed = $Prefix + $_.Name
+                Copy-Item $_ -Destination $Path\$Prefixed -Force
+            }
+        }
     }
-    Remove-Item $temp\lw -Recurse -Force
 }
 
-Install-Uri $aviutl
-Install-Uri $exedit
-Install-Uri-lw (Get-GithubLatestUrl $lw $lw_KeyWord)
+# URLs
+[String]$aviutl = "http://spring-fragrance.mints.ne.jp/aviutl/aviutl110.zip"
+[String]$exedit = "http://spring-fragrance.mints.ne.jp/aviutl/exedit92.zip"
+[String]$lw = Get-GithubLatestUrl "https://api.github.com/repos/Mr-Ojii/L-SMASH-Works-Auto-Builds/releases" "Mr-Ojii_AviUtl.zip"
+[String]$InputPipePlugin = Get-GithubLatestUrl "https://api.github.com/repos/amate/InputPipePlugin/releases"
+[String]$easymp4 = "https://aoytsk.blog.jp/aviutl/easymp4.zip"
+[String]$patchaul = "https://scrapbox.io/files/6242bf590ea51d001d275052.zip"
+[String]$luajit = Get-GithubLatestUrl "https://api.github.com/repos/Per-Terra/LuaJIT-Auto-Builds/releases" "Win_x86.zip"
+
+# 処理ここから
+[String]$root = New-DirectryIfNotExist $InstallPath
+[String]$plugins = New-DirectryIfNotExist (Join-Path $InstallPath 'plugins')
+[String]$sclipt = New-DirectryIfNotExist (Join-Path $InstallPath 'script')
+
+Install-UriArchive $aviutl $root
+Install-UriArchive $exedit $root
+Install-UriArchive $lw $plugins -Filter "*.au*" -Prefix "lw_" -Exclude "Licenses"
+Install-UriArchive $InputPipePlugin $plugins -Filter "*.au*" -Prefix "InputPipePlugin_"
+Install-UriArchive $easymp4 $plugins -Filter "easymp4*" -Prefix "easymp4_"
+Install-UriArchive $patchaul $root
+Install-UriArchive $luajit $root -Filter "lua51.dll"
+
+if (!$KeepForeignData) {
+    Remove-Item $WorkingDirectry -Recurse -Force
+}
